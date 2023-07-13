608929182d20a28c299cd3dc8b8017dfef0134cc
unikuma <85827428+unikuma@users.noreply.github.com>
2023-07-13 14:05:05 +0900

    BouyomisanEngineクラスにシングルトンパターンを適用

--- Bouyomisan/Models/BouyomisanEngine.cs
+++ Bouyomisan/Models/BouyomisanEngine.cs
@@ -2,7 +2,17 @@
 
 namespace Bouyomisan.Models
 {
+    /// <summary>
+    /// 棒読みさんの核となる音声生成・字幕生成を担当するクラスです。
+    /// </summary>
     public class BouyomisanEngine : NotificationObject
     {
+        public static BouyomisanEngine Instance
+        {
+            get => _instance;
+        }
+
+        private BouyomisanEngine() { }
+        private static readonly BouyomisanEngine _instance = new();
     }
 }
