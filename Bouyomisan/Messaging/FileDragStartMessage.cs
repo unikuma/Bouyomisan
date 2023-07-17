ded641af6a087294b300aa75c391694b21feeb4e
unikuma <85827428+unikuma@users.noreply.github.com>
2023-07-17 17:06:22 +0900

    FileDragStartMessage・Actionを追加

---
+++ Bouyomisan/Messaging/FileDragStartMessage.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using Livet.Messaging;
+
+namespace Bouyomisan.Messaging
+{
+    public class FileDragStartMessage : InteractionMessage
+    {
+        public FileDragStartMessage(string targetFile) : base()
+        {
+            TargetFile = targetFile;
+        }
+
+        public FileDragStartMessage(string targetFile, string messageKey) : base(messageKey)
+        {
+            TargetFile = targetFile;
+        }
+
+        public string TargetFile { get; init; }
+
+        /// <summary>
+        /// Please do not remove this method.
+        /// </summary>
+        /// <returns>A new instance of this.</returns>
+        protected override Freezable CreateInstanceCore()
+        {
+            return new FileDragStartMessage(TargetFile, MessageKey);
+        }
+    }
+}
---
+++ Bouyomisan/Messaging/FileDragStartMessageAction.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Livet.Behaviors.Messaging;
+using Livet.Messaging;
+
+namespace Bouyomisan.Messaging
+{
+    public class FileDragStartMessageAction : InteractionMessageAction<FrameworkElement>
+    {
+        protected override void InvokeAction(InteractionMessage m)
+        {
+            if (m is FileDragStartMessage message)
+            {
+                DragDrop.DoDragDrop(
+                    AssociatedObject,
+                    new DataObject(DataFormats.FileDrop, new string[] { message.TargetFile }),
+                    DragDropEffects.Copy);
+            }
+        }
+    }
+}
